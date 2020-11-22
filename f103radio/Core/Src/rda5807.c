05552ec29f2b67698fd4cb5c861f5b5fac6923cf
miszczo <miszczo997@gmail.com>
2020-11-22 22:06:49 +0100

    fixed i2c addr, something is working

--- f103radio/Core/Src/rda5807.c
+++ f103radio/Core/Src/rda5807.c
@@ -13,6 +13,11 @@ I2C_HandleTypeDef rdahi2c;
 rda5807_config_t rda5807_config;
 rda5807_status_t rda5807_status;
 
+uint16_t frequency;
+uint16_t band;
+uint8_t space;
+uint8_t volume;
+
 
 
 int8_t rda5807_init(I2C_HandleTypeDef *i2c_h) {
@@ -24,40 +29,40 @@ int8_t rda5807_init(I2C_HandleTypeDef *i2c_h) {
 
 
 
+  rda5807_config.reg02.refined.SEEKUP = 1;
 
   // set init values of registers
-  rda5807_config.reg02.refined.MONO = 1;
-  /*
+  //rda5807_config.reg02.refined.MONO = 1;
+
   rda5807_config.reg02.refined.DMUTE = 1;
   rda5807_config.reg02.refined.DHIZ = 1;
   rda5807_config.reg02.refined.ENABLE = 1;
+  rda5807_config.reg02.refined.SEEK = 1;
 
   rda5807_config.reg05.refined.INT_MODE = 0;
   rda5807_config.reg05.refined.LNA_PORT_SEL = 2;
   rda5807_config.reg05.refined.LNA_ICSEL_BIT = 0;
   rda5807_config.reg05.refined.SEEKTH = 8;
-  rda5807_config.reg05.refined.VOLUME = 0b1011;
-*/
+  rda5807_config.reg05.refined.VOLUME = 0b0001;
 
-  dbg("mono %x\n\r",rda5807_config.reg00.refined.DUMMY);
 
-  dbg("r0 = %x\n\r",rda5807_config.reg00.raw);
-  dbg("r1 = %x\n\r",rda5807_config.reg01.raw);
-  dbg("r2 = %x\n\r",rda5807_config.reg02.raw);
-  dbg("r3 = %x\n\r",rda5807_config.reg03.raw);
-  dbg("r4 = %x\n\r",rda5807_config.reg04.raw);
-  dbg("r5 = %x\n\r",rda5807_config.reg05.raw);
-  dbg("r6 = %x\n\r",rda5807_config.reg06.raw);
-  dbg("r7 = %x\n\r",rda5807_config.reg07.raw);
+  rda5807_config.reg03.refined.CHAN = 0x15F;
+
 
 
 
   //rda5807_config.reg00.refined.CHIPID = 0x04;
   // TODO:
-  //rda5807_write_register(0x02, rda5807_config.reg02.raw);
-  //rda5807_write_register(0x02, rda5807_config.reg05.raw);
+  //rda5807_write_register(0x02, 2); // software reset chip;
+  rda5807_write_register(0x03, rda5807_config.reg03.raw);
+  rda5807_write_register(0x02, rda5807_config.reg02.raw);
+  rda5807_write_register(0x05, rda5807_config.reg05.raw);
+  rda5807_write_register(0x02, rda5807_config.reg02.raw);
 
-  rda5807_write_register(0x02, 0b1111000000000001);
+  HAL_Delay(500);
+
+
+  //rda5807_write_register(0x02, 0b1111000000000001);
   //rda5807_write_register(0x05, 0b0000100010111111);
 
   return RDA5807_OK;
@@ -80,21 +85,9 @@ int8_t rda5807_write_register(uint8_t reg, uint16_t val) {
   data[2] = (uint8_t)val & 0xFF;
 
 
-
-
-
-
-
-
-
-
-  dbg("sizeof s truct = %d\n\r",sizeof(rda5807_config.reg02));
-  dbg("r = %x\n\r", val);
-  dbg("1=%x 2=%x=3%x\n\r", data[0],data[1],data[2]);
-  //while(1);
   // send data to rda5807
   HAL_StatusTypeDef err;
-  err = HAL_I2C_Master_Transmit(&rdahi2c, RDA5807_DIRECT_WRITE_REGISTER << 1,
+  err = HAL_I2C_Master_Transmit(&rdahi2c, 0x11 << 1,
                                  data, 3, 300);
   if(err!=HAL_OK)
     return RDA5807_WRITE_ERROR;
